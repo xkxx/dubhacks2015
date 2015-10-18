0a0515dbfb03ecfb56e91d088f45828ded1ae9e7
Julia Chu <juliachu@D-173-250-240-106.dhcp4.washington.edu>
2015-10-17 21:32:58 -0700

    added server.py

---
+++ server.py
@@ -0,0 +1,25 @@
+from bottle import route, run, error, static_file
+from bottle import request as req
+
+@route('/')
+def serveHTML():
+    return static_file('app.html', root="./static/")
+
+@route('/js/<file>')
+def serveJS(file):
+    return static_file(file, root="./static/js")
+
+@route('/css/<file>')
+def serveCSS(file):
+    return static_file(file, root="./static/css")
+
+@route('/font/<file>')
+def serveCSS(file):
+    return static_file(file, root="./static/font")
+
+@error(404)
+def err404(err):
+    return "404 File Not Found"
+
+# run(host='0.0.0.0', port=80, debug=False)
+run(host='localhost', port=8080, debug=True)
