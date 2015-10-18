2f6d34f417218653526547e5eb6b0377df784910
juliachu0825 <juliachu0825@gmail.com>
2015-10-18 15:28:37 -0700

    service update

--- service.py
+++ service.py
@@ -1,16 +1,20 @@
 import redis
 import operator
 import ast
+import userCustomize
 
 LENGTH_WEIGHT = 1.5
 r = redis.StrictRedis(db=1)
 
 def getData(input):
+
     allword = []
     wordList = input.split()
     index = len(wordList) - 1
     curr = wordList[index]
     while True:
+        if index > 0:
+            userCustomize.userCustomize(wordList[index - 1], curr)
         if r.exists(curr) >= 1:
             result = r.zrevrange(curr, 0, 5, withscores=True)
             for element in result:
