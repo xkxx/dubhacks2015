067fde18f43252d6bafa725f7256c5ba8561be76
Minjing Zhu <minjiz@uw.edu>
2015-10-18 16:45:36 -0700

    fix

--- userCustomize.py
+++ userCustomize.py
@@ -4,5 +4,5 @@ import nltk
 pool_ho = redis.ConnectionPool(host='localhost', port=6379, db=3)
 
 def userCustomize(hint, optionstr):
-    redis_ho = redis.Redis(connection_pool=pool_ho)
-    redis_ho.zincrby(hint, nltk.word_tokenize(optionstr), 1)
+   # redis_ho = redis.Redis(connection_pool=pool_ho)
+   # redis_ho.zincrby(hint, nltk.word_tokenize(optionstr), 1)
