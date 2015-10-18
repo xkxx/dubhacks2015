10533333897d9a755b0f7157fe454cc0f45b746a
Minjing Zhu <minjiz@uw.edu>
2015-10-18 01:24:17 -0700

    modified Ngram Builder

---
+++ parser/NGramBuiler.py
@@ -0,0 +1,36 @@
+from nltk.util import ngrams
+import nltk
+from os import listdir
+import re
+
+N = 5  # ngram from 2 to (N - 1)
+DATA_DIR = 'dataset/'
+REGEX = "^[(0-9)\W]+|com|http$"
+
+NGram = list()
+
+def everygrams(tokens):
+    retlist = list()
+    for i in range(2, N):
+        generated_ngram = list(ngrams(tokens, i))
+        retlist.extend(generated_ngram)
+    return retlist
+
+files = [open(DATA_DIR + filename).read()
+    for filename in listdir(DATA_DIR)]
+
+sentences = list()
+for lines in [file.split('\n') for file in files]:
+    for line in lines:
+        lineTokens = line.split('\t')
+        if len(lineTokens) > 1:
+            sentences.append(lineTokens[1])
+
+for sentence in sentences:
+    tokens = nltk.word_tokenize(sentence)
+    pattern = re.compile(REGEX)
+    validWords = [token for token in tokens if not pattern.match(token)]
+    NGram.extend(everygrams(validWords))
+
+def getNGram():
+    return NGram
