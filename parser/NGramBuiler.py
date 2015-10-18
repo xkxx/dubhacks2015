fd1b5973553d7a74137a081ad40f54b48907c488
Minjing Zhu <minjiz@uw.edu>
2015-10-18 01:48:41 -0700

    go to sleep

--- parser/NGramBuiler.py
+++ parser/NGramBuiler.py
@@ -2,12 +2,15 @@ from nltk.util import ngrams
 import nltk
 from os import listdir
 import re
+import redis
 
 N = 5  # ngram from 2 to (N - 1)
-DATA_DIR = 'dataset/'
+DATA_DIR = 'testdataset/'
 REGEX = "^[(0-9)\W]+|com|http$"
 
 NGram = list()
+
+rd = redis.StrictRedis()
 
 def everygrams(tokens):
     retlist = list()
@@ -16,21 +19,27 @@ def everygrams(tokens):
         retlist.extend(generated_ngram)
     return retlist
 
-files = [open(DATA_DIR + filename).read()
-    for filename in listdir(DATA_DIR)]
+def buildNGram():
+    files = [open(DATA_DIR + filename).read()
+        for filename in listdir(DATA_DIR)]
 
-sentences = list()
-for lines in [file.split('\n') for file in files]:
-    for line in lines:
-        lineTokens = line.split('\t')
-        if len(lineTokens) > 1:
-            sentences.append(lineTokens[1])
+    sentences = list()
+    for lines in [file.split('\n') for file in files]:
+        for line in lines:
+            lineTokens = line.split('\t')
+            if len(lineTokens) > 1:
+                sentences.append(lineTokens[1])
 
-for sentence in sentences:
-    tokens = nltk.word_tokenize(sentence)
-    pattern = re.compile(REGEX)
-    validWords = [token for token in tokens if not pattern.match(token)]
-    NGram.extend(everygrams(validWords))
+    for sentence in sentences:
+        tokens = nltk.word_tokenize(sentence)
+        pattern = re.compile(REGEX)
+        validWords = [token for token in tokens if not pattern.match(token)]
+        NGram.extend(everygrams(validWords))
 
 def getNGram():
     return NGram
+
+def serializeNGram():
+    for gram in NGram:
+        rd.incr(gram)
+    
