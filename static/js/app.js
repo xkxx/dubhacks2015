8117908af611b86e6f2884dc9703e8a4b499c893
xkxx <xukexiang@gmail.com>
2015-10-17 20:21:29 -0700

    fisi

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
 var ProseMirror = require("prosemirror/dist/edit").ProseMirror
-require("prosemirror/dist/menu/menubar") // Load menubar module
 
 var subjectEditor = new ProseMirror({
   place: document.getElementById("subject"),
