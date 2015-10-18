5465d4ceddf8007762baa6dc3e36b854db1d0e81
Minjing Zhu <minjiz@uw.edu>
2015-10-18 13:58:15 -0700

    database

--- parser/parallelTrainer.py
+++ parser/parallelTrainer.py
@@ -1,21 +1,22 @@
 from nltk.util import ngrams
 import nltk
 from os import listdir
+import sys
 import re
 import redis
 import ast
 import threading
 
-N = 4  # ngram from 2 to (N - 1)
-DATA_DIR = 'testdataset/'
-REGEX = "^[(0-9)\W]+|com|http$"
+N = 6  # ngram from 2 to (N - 1)
+DATA_DIR = 'dataset/'
+REGEX = "^[(0-9)\W]+|com|http|([^\x00-\x7F]+)$"
 FREQUENCY_THRESHOLD = 2
 THREAD_NUM = 8
 
 NGram = list()
 
 pool_ngram = redis.ConnectionPool(host='localhost', port=6379, db=2)
-pool_ho = redis.ConnectionPool(host='localhost', port=6379, db=2)
+pool_ho = redis.ConnectionPool(host='localhost', port=6379, db=3)
 
 def everygrams(tokens):
     retlist = list()
@@ -26,15 +27,21 @@ def everygrams(tokens):
 
 def ngramWorker(i, sentences):
     redis_ngram = redis.Redis(connection_pool=pool_ngram)
-    workset = sentences[i * (len(sentences) / THREAD_NUM):
-        (i + 1) * (len(sentences) / THREAD_NUM)]
+    lo = i * len(sentences) / THREAD_NUM
+    hi = (i + 1) * len(sentences) / THREAD_NUM
+    workset = sentences[lo:hi]
     for sentence in workset:
-        tokens = nltk.word_tokenize(sentence)
-        pattern = re.compile(REGEX)
-        validWords = [token for token in tokens if not pattern.match(token)]
-        everygram = everygrams(validWords)
-        for gram in everygram:
-            redis_ngram.incr(gram)
+        try:
+            sentence = sentence.decode('unicode_escape').encode('ascii', 'ignore')
+            tokens = nltk.word_tokenize(sentence)
+            pattern = re.compile(REGEX)
+            validWords = [token for token in tokens
+                if not pattern.match(token)]
+            everygram = everygrams(validWords)
+            for gram in everygram:
+                redis_ngram.incr(gram)
+        except UnicodeDecodeError:
+            print 'encode error ignored'
 
 def buildNGram():
     files = [open(DATA_DIR + filename).read()
@@ -53,15 +60,13 @@ def buildNGram():
         threads.append(t)
         t.start()
 
-def getNGram():
-    return NGram
-
 # scanForCommonGramsAndBuildHintOptionsStructure
 def incrHintOptionWorker(i, grams):
     redis_ho = redis.Redis(connection_pool=pool_ho)
     redis_ngram = redis.Redis(connection_pool=pool_ngram)
-    size = len(grams)
-    for gram in grams[i * (size / THREAD_NUM): (i + 1) * (size / THREAD_NUM)]:
+    lo = i * len(grams) / THREAD_NUM
+    hi = (i + 1) * len(grams) / THREAD_NUM
+    for gram in grams[lo:hi]:
         freq = redis_ngram.get(gram)
         if freq >= FREQUENCY_THRESHOLD:
             # save hint -> option pair to redis
@@ -77,10 +82,14 @@ def trainModel():
     for i in range(THREAD_NUM):
         t = threading.Thread(target=incrHintOptionWorker, args=(i, grams))
         threads.append(t)
+        t.start()
 
 def userCustomize(hint, options):
     redis_ho = redis.Redis(connection_pool=pool_ho)
     redis_ho.zincrby(hint, options, 3)
 
-buildNGram()
-trainModel()
+if (sys.argv[1] == 'build'):
+    buildNGram()
+
+if (sys.argv[1] == 'train'):
+    trainModel()
